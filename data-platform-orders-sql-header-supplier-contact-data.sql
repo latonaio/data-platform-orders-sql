3470e946a0e4826520b5de38828277d40ad7ae41
MiyuYokoyama0109 <miyu.y@latona.io>
2022-10-18 19:10:17 +0900

    release

--- data-platform-orders-sql-header-supplier-contact-data.sql
+++ data-platform-orders-sql-header-supplier-contact-data.sql
@@ -1,5 +1,6 @@
 CREATE TABLE `data_platform_orders_header_supplier_contact_data`
 (
+  `BusinessPartner`               int(10) NOT NULL,            -- 新規追加
   `OrderID`                       int(10) NOT NULL,            -- 新規追加
   `Supplier`                      int(10) NOT NULL,            -- 新規追加
   `ContactID`                     int(4) NOT NULL,             -- 新規追加
@@ -12,7 +13,7 @@ CREATE TABLE `data_platform_orders_header_supplier_contact_data`
   `ContactTag2`                   varchar(40) DEFAULT NULL,    -- 新規追加
   `ContactTag3`                   varchar(40) DEFAULT NULL,    -- 新規追加
   `ContactTag4`                   varchar(40) DEFAULT NULL,    -- 新規追加 
-  PRIMARY KEY (`OrderID`, `Supplier`, `ContactID`) ,
-  CONSTRAINT `DataPlatformOrdersHeaderSupplierContactData_fk` FOREIGN KEY (`OrderID`, `Supplier`) REFERENCES `data_platform_orders_header_data` (`OrderID`, `Supplier`)
+  PRIMARY KEY (`BusinessPartner`, `OrderID`, `Supplier`, `ContactID`) ,
+  CONSTRAINT `DataPlatformOrdersHeaderSupplierContactData_fk` FOREIGN KEY (`BusinessPartner`, `OrderID`, `Supplier`) REFERENCES `data_platform_orders_header_data` (`BusinessPartner`, `OrderID`, `Supplier`)
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
