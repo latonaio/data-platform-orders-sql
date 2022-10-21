b5b06625eaf7ad1c4ff0f2d1ab704db89034dc0d
MiyuYokoyama0109 <miyu.y@latona.io>
2022-10-21 18:54:39 +0900

    release

--- data-platform-orders-sql-item-partner-data.sql
+++ data-platform-orders-sql-item-partner-data.sql
@@ -4,7 +4,10 @@ CREATE TABLE `data_platform_orders_item_partner_data`
   `OrderID`                             int(10) NOT NULL,      -- 名称変更
   `OrderItem`                           int(6) NOT NULL,       -- 名称変更
   `PartnerFunction`                     varchar(2) NOT NULL,
-  `PartnerFunctionBusinessPartner`      int(10) DEFAULT NULL,  
+  `PartnerFunctionBusinessPartner`      int(10) DEFAULT NULL,
+  `BusinessPartnerFullName`             varchar(200) DEFAULT NULL,  -- 新規追加
+  `BusinessPartnerName`                 varchar(100) DEFAULT NULL,  -- 新規追加
+  `AddressID`                           int(10) DEFAULT NULL,       -- 新規追加
     PRIMARY KEY (`BusinessPartner`, `OrderID`, `OrderItem`, `PartnerFunction`),
     CONSTRAINT `DataPlatformOrdersItemPartnerData_fk` FOREIGN KEY (`BusinessPartner`, `OrderID`, `OrderItem`) REFERENCES `data_platform_orders_item_data` (`BusinessPartner`, `OrderID`, `OrderItem`)
 ) ENGINE = InnoDB
